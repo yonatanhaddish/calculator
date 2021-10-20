d582edad3d708e7f166a8a6a9b84fce2cf7ef564
Yonatan Haddish <yonihailshn@gmail.com>
2021-10-20 16:36:48 -0400

    button handlers

--- src/App.js
+++ src/App.js
@@ -3,30 +3,34 @@ import './App.css';
 
 function App() {
   const [result, setResult]= useState("");
+
+  const handleClick = (e) => {
+    console.log(e.target.name);
+  }
   return (
       <div className="container">
         <form>
           <input type="text" value={result}></input>
         </form>
         <div className="keypad">
-          <button>Clear</button>
-          <button>C</button>
-          <button>&divide;</button>
-          <button>7</button>
-          <button>8</button>
-          <button>9</button>
-          <button>&times;</button>
-          <button>4</button>
-          <button>5</button>
-          <button>6</button>
-          <button>&ndash;</button>
-          <button>1</button>
-          <button>2</button>
-          <button>3</button>
-          <button>+</button>
-          <button>0</button>
-          <button>.</button>
-          <button>=</button>
+          <button onClick={handleClick}>Clear</button>
+          <button onClick={handleClick}>C</button>
+          <button onClick={handleClick}>&divide;</button>
+          <button onClick={handleClick}>7</button>
+          <button onClick={handleClick}>8</button>
+          <button onClick={handleClick}>9</button>
+          <button onClick={handleClick}>&times;</button>
+          <button onClick={handleClick}>4</button>
+          <button onClick={handleClick}>5</button>
+          <button onClick={handleClick}>6</button>
+          <button onClick={handleClick}>&ndash;</button>
+          <button onClick={handleClick}>1</button>
+          <button onClick={handleClick}>2</button>
+          <button onClick={handleClick}>3</button>
+          <button onClick={handleClick}>+</button>
+          <button onClick={handleClick}>0</button>
+          <button onClick={handleClick}>.</button>
+          <button onClick={handleClick}>=</button>
         </div>
       </div>
   );
