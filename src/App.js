85754c681f15a93846e1a9ac1e440b2e63836abb
Yonatan Haddish <yonihailshn@gmail.com>
2021-10-20 18:25:50 -0400

    buttons styled

--- src/App.js
+++ src/App.js
@@ -23,24 +23,24 @@ function App() {
           <input type="text" value={result}></input>
         </form>
         <div className="keypad">
-          <button onClick={clear} id="clear">Clear</button>
-          <button onClick={backspace} id="backspace">C</button>
-          <button name="/" onClick={handleClick}>&divide;</button>
+          <button onClick={clear} className="highlight" id="clear">Clear</button>
+          <button onClick={backspace} className="highlight" id="backspace">C</button>
+          <button name="/" onClick={handleClick} className="highlight">&divide;</button>
           <button name="7" onClick={handleClick}>7</button>
           <button name="8" onClick={handleClick}>8</button>
           <button name="9" onClick={handleClick}>9</button>
-          <button name="*" onClick={handleClick}>&times;</button>
+          <button name="*" onClick={handleClick} className="highlight">&times;</button>
           <button name="4" onClick={handleClick}>4</button>
           <button name="5" onClick={handleClick}>5</button>
           <button name="6" onClick={handleClick}>6</button>
-          <button name="-" onClick={handleClick}>&ndash;</button>
+          <button name="-" onClick={handleClick} className="highlight">&ndash;</button>
           <button name="1" onClick={handleClick}>1</button>
           <button name="2" onClick={handleClick}>2</button>
           <button name="3" onClick={handleClick}>3</button>
-          <button name="+" onClick={handleClick}>+</button>
+          <button name="+" onClick={handleClick} className="highlight">+</button>
           <button name="0" onClick={handleClick}>0</button>
           <button name="." onClick={handleClick}>.</button>
-          <button name="=" onClick={handleClick} id="result">=</button>
+          <button name="=" onClick={handleClick} className="highlight" id="result">=</button>
         </div>
       </div>
   );
